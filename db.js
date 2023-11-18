485bbbd49a8d8e8b63d9b81f057ad9e53697c7cb
Raajulogu <rajeshkumarlogu145@gmail.com>
2023-11-18 22:36:42 +0530

    Database commit

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ export function dbconnection(){
         useUnifiedTopology: true,
     }
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/recipe_keeper",params);
+        mongoose.connect("mongodb+srv://rajesh:rajesh145@cluster0.563jw0h.mongodb.net/Recipe_Keeper?retryWrites=true&w=majority",params);
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Error connecting DB ---", error);
